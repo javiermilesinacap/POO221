a98dd5506af5fa33e2d759515d0de31e5191a5cd
javiermilesinacap <109394677+javiermilesinacap@users.noreply.github.com>
2022-10-28 15:15:16 -0300

    Update agario.py

--- agario.py
+++ agario.py
@@ -18,14 +18,42 @@ class Player:
             self.y -= self.vel
         if keys[pygame.K_DOWN]:
             self.y += self.vel
+        mouse = pygame.mouse.get_pos()
+        if mouse[0] < self.x:
+            self.x -= self.vel
+        if mouse[0] > self.x:
+            self.x += self.vel
+        if mouse[1] < self.y:
+            self.y -= self.vel
+        if mouse[1] > self.y:
+            self.y += self.vel
+class Enemy: 
+    def __init__(self, x, y, radius, color):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.color = color
+        self.vel = 3
+    def draw(self, win):
+        pygame.draw.circle(win, self.color, (self.x, self.y), self.radius)
+    def move(self):
+        self.x += self.vel
+        self.y += self.vel
+        if self.y > 500 - self.radius or self.y < self.radius:
+            self.vel *= -1
+        if self.x > 500 - self.radius or self.x < self.radius:
+            self.vel *= -1
+
 def redrawGameWindow():
     win.fill((255,255,255))
     player.draw(win)
+    enemy.draw(win)
     pygame.display.update()
 pygame.init()
 win = pygame.display.set_mode((500, 500))
 pygame.display.set_caption("Agario")
 player = Player(250, 250, 50, (255, 0, 0))
+enemy = Enemy(100, 100, 50, (0, 255, 0))
 run = True
 while run:
     pygame.time.delay(50)
@@ -33,5 +61,6 @@ while run:
         if event.type == pygame.QUIT:
             run = False
     player.move()
+    enemy.move()
     redrawGameWindow()
 pygame.quit()
